6eab47b8879dec0318338fb54743a3492257340f
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-09-05 09:58:43 +0200

    made unit_main non-recursive to fix menu control

--- convert.c
+++ convert.c
@@ -3,7 +3,8 @@
 #include "convert.h"
 #include "common.h"
 
-int main(){
+int main()
+{
     display_intro();
 
     // Infinite loop - breaks when user chooses to exit
@@ -11,12 +12,12 @@ int main(){
     {
         display_main_menu();
         char choice = get_choice();
-        
+
         switch (choice)
         {
         case 't':
-        case'v':
-        case'p':
+        case 'v':
+        case 'p':
             unit_main(choice);
             break;
         case 'x':
@@ -28,57 +29,68 @@ int main(){
     }
 }
 
-int unit_main(char unit_type){
-    
+int unit_main(char unit_type)
+{
+
     char choices[2];
     char valid_input[3];
     switch (unit_type)
     {
-        case 't':
-            display_temperature_menu();
-            char temp_valid_input[] = {'f', 'c', 'k'};
-            memcpy(valid_input, temp_valid_input, sizeof(temp_valid_input));
-            break;
-        case 'v':
-            display_volume_menu();
-            char vol_valid_input[] = {'l', 'g', 't'};
-            memcpy(valid_input, vol_valid_input, sizeof(vol_valid_input));
-            break;
-        case 'p':
-            display_pressure_menu();
-            char pressure_valid_input[] = {'b', 'p', 'k'};
-            memcpy(valid_input, pressure_valid_input, sizeof(pressure_valid_input));
-            break;
+    case 't':
+        display_temperature_menu();
+        char temp_valid_input[] = {'f', 'c', 'k'};
+        memcpy(valid_input, temp_valid_input, sizeof(temp_valid_input));
+        break;
+    case 'v':
+        display_volume_menu();
+        char vol_valid_input[] = {'l', 'g', 't'};
+        memcpy(valid_input, vol_valid_input, sizeof(vol_valid_input));
+        break;
+    case 'p':
+        display_pressure_menu();
+        char pressure_valid_input[] = {'b', 'p', 'k'};
+        memcpy(valid_input, pressure_valid_input, sizeof(pressure_valid_input));
+        break;
     }
 
     // // (r) has been entered while getting units - return to main menu
-    if (get_unit_choices(choices, valid_input) == 0) {
+    if (get_unit_choices(choices, valid_input) == 0)
+    {
         return 0;
     }
 
     // Repeat until break or return is hit
-    while (1) {
-        if (unit_type == 'v') {
+    while (1)
+    {
+        if (unit_type == 'v')
+        {
             volume_main(choices);
         }
-        else if (unit_type == 't'){
+        else if (unit_type == 't')
+        {
             temperature_main(choices);
         }
-        else if (unit_type == 'p'){
+        else if (unit_type == 'p')
+        {
             pressure_main(choices);
         }
-        
-       ask_repeat();
-       char shouldRepeat = get_choice();
 
-       // Get new units of the same type
-       if (shouldRepeat == 'n') {
-        unit_main(unit_type);
-        // break;
-       }
-       // Returns to the "main menu"
-       else if (shouldRepeat == 'r') return 0;
+        ask_repeat();
+        char shouldRepeat = get_choice();
+
+        // Get new units of the same type
+        if (shouldRepeat == 'n')
+        {
+            if (get_unit_choices(choices, valid_input) == 0) {
+                return 0;
+            }
+            // unit_main(unit_type);
+            // break;
+        }
+        // Returns to the "main menu"
+        else if (shouldRepeat == 'r')
+            return 0;
     }
-    
+
     return 0;
 }
